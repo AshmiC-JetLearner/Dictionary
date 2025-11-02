8a76ea6141a41e3c9461c1c67eefd832a627d7ef
AshmiC-JetLearner <ashmi.c32@gmail.com>
2025-11-02 12:58:39 -0500

    Create Dicti2.py

---
+++ Dicti2.py
@@ -0,0 +1,14 @@
+dict1={'name':'Ashmi','age':11,'country':'America','currency':'Dollars'}
+print(dict1,'\n')
+dict1['Marks']=[90,88,97,100,95]
+print(dict1,'\n')
+
+#get the age 
+print(f'age ={dict1['age']}\n')
+
+#get marks list
+print(f'marks={dict1['Marks']}\n')
+
+#get the 4th index marks from the marks list
+print(f'4th mark={dict1['Marks'][3]}\n')
+print(f'1st mark={dict1['Marks'][0]}\n')
